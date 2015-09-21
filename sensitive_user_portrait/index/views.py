e6c74228cb9df349e59c26f03639e56972578cdd
linhaobuaa <1257819385@qq.com>
2015-09-21 17:36:15 +0800

    ljh

--- sensitive_user_portrait/index/views.py
+++ sensitive_user_portrait/index/views.py
@@ -7,7 +7,7 @@ from sensitive_user_portrait.time_utils import ts2datetime, ts2date
 from flask import Blueprint, url_for, render_template, request, abort, flash, session, redirect, make_response
 
 
-mod = Blueprint('portrait', __name__, url_prefix='/index')
+mod = Blueprint('sensitive', __name__, url_prefix='/index')
 
 
 @mod.route('/')
@@ -18,5 +18,37 @@ def loading():
 @mod.route('/overview/')
 def overview():
     return render_template('overview.html')
+
+@mod.route('/recommend_in/')
+def recommend_in():
+    return render_template('recommend_in.html')
+
+@mod.route('/tag/')
+def tag():
+    return render_template('tag.html')
+
+@mod.route('/sensitive_words/')
+def sensitive_words():
+    return render_template('sensitive.html')
+
+@mod.route('/group/')
+def group():
+    return render_template('group.html')
+
+@mod.route('/search/')
+def search():
+    return render_template('search.html')
+
+@mod.route('/influence/')
+def influence():
+    return render_template('influence.html')
+
+@mod.route('/monitor/')
+def monitor():
+    return render_template('monitor.html')
+
+@mod.route('/contrast/')
+def contrast():
+    return render_template('contrast.html')
 
 
