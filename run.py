6e5a5568dd3a7fd8bd946cb6af5e284abbb192bb
linhaobuaa <1257819385@qq.com>
2015-09-21 11:18:44 +0800

    zhang

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from optparse import OptionParser
 
-from user_portrait import create_app
+from sensitive_user_portrait import create_app
 
 optparser = OptionParser()
 optparser.add_option('-p', '--port', dest='port', help='Server Http Port Number', default=9001, type='int')
