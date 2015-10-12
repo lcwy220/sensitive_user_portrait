2bcec1f60a8fdece922c5012e34384d4d51dff2f
linhaobuaa <1257819385@qq.com>
2015-10-12 21:41:55 +0800

    ljh

--- sensitive_user_portrait/static/custom/js/group.js
+++ sensitive_user_portrait/static/custom/js/group.js
@@ -26,7 +26,7 @@ function Draw_resultTable(data){
 		}else{
 			html += '<td><a style="cursor:hand;" href="/index/group_analysis/?name=' + item[i][0]+ '">监控停止</a></td>';
 		}
-		html +='<td><a href="javascript:void(0)" id="del">删除</a><a href="javascript:void(0)" id="stop"></a></td>';
+		html +='<td><a href="javascript:void(0)" id="del">删除</a><a href="javascript:void(0)" id="stop">停止监控</a></td>';
 		html += '</tr>';
 	}
 	html += '</tbody>';
