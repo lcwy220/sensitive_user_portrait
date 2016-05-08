79f04720a35706e5f3c8df691d1fe2daf92d494f
linhaobuaa <1257819385@qq.com>
2016-05-08 15:01:32 +0800

    yuankun

--- sensitive_user_portrait/__init__.py
+++ sensitive_user_portrait/__init__.py
@@ -5,19 +5,21 @@ from elasticsearch import Elasticsearch
 from flask_debugtoolbar import DebugToolbarExtension
 from sensitive_user_portrait.extensions import admin
 from sensitive_user_portrait.jinja import gender, tsfmt, Int2string, gender_text, user_email, user_location, user_birth, user_vertify, weibo_source
+from sensitive_user_portrait.social_sensing.views import mod as socialsensingModule
 from sensitive_user_portrait.index.views import mod as indexModule
 from sensitive_user_portrait.recommentation.views import mod as recommentationModule
 from sensitive_user_portrait.group.views import mod as groupModule
 from sensitive_user_portrait.tag.views import mod as tagModule
-from sensitive_user_portrait.attribute.views import mod as attributeModule
+#from sensitive_user_portrait.attribute.views import mod as attributeModule
 from sensitive_user_portrait.overview.views import mod as overviewModule
 from sensitive_user_portrait.imagine.views import mod as imagineModule
-from sensitive_user_portrait.search.views import mod as searchModule
+#from sensitive_user_portrait.search.views import mod as searchModule
 from sensitive_user_portrait.manage_sensitive_words.views import mod as wordsmanagementModule
 from sensitive_user_portrait.delete_user.views import mod as deleteModule
 from sensitive_user_portrait.influence_application.views import mod as influenceModule
 from sensitive_user_portrait.user_rank.views import mod as userrankModule
 from sensitive_user_portrait.weibo_rank.views import mod as weiborankModule
+from sensitive_user_portrait.portrait_search.views import mod as searchModule
 """
 from sensitive_user_portrait.attribute.views import mod as attributeModule
 from sensitive_user_portrait.manage.views import mod as manageModule
@@ -74,7 +76,7 @@ def register_blueprints(app):
     app.register_blueprint(recommentationModule)
     app.register_blueprint(groupModule)
     app.register_blueprint(tagModule)
-    app.register_blueprint(attributeModule)
+    #app.register_blueprint(attributeModule)
     app.register_blueprint(overviewModule)
     app.register_blueprint(imagineModule)
     app.register_blueprint(searchModule)
@@ -83,6 +85,7 @@ def register_blueprints(app):
     app.register_blueprint(influenceModule)
     app.register_blueprint(userrankModule)
     app.register_blueprint(weiborankModule)
+    app.register_blueprint(socialsensingModule)
     """
     app.register_blueprint(attributeModule)
     app.register_blueprint(manageModule)
