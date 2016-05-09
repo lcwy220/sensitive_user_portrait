3af89f21d6b049577abd1b2bbf44a8eafd26359d
linhaobuaa <1257819385@qq.com>
2016-05-09 21:48:08 +0800

    zhagn

--- sensitive_user_portrait/static/js/portrait/search_portrait.js
+++ sensitive_user_portrait/static/js/portrait/search_portrait.js
@@ -5,38 +5,40 @@ function draw_search_results(data){
     //console.log(user_url);
     var html = '';
     html += '<table class="table table-striped table-bordered bootstrap-datatable datatable responsive">';
-    html += '<thead><tr><th>用户ID</th><th>昵称</th><th>注册地</th><th>活跃度</th><th>重要度</th><th>影响力</th><th>相关度</th>';
+    html += '<thead><tr><th>用户ID</th><th>昵称</th><th>注册地</th><th>活跃度</th><th>身份敏感度</th><th>重要度</th><th>影响力</th><th>相关度</th>';
     html += '<th>操作</th></tr></thead>';
     html += '<tbody>';
-    for(var i = 0; i<6;i++){
+    for(var i = 0;i<data.length;i++){
       //var item = data[i];
 	  var item = data;
       //item = replace_space(item);
-	  /*
+	  
       if (item[1] == '未知'){
           item[1] = item[0];
       } 
+	  /*
       for(var j=3;j<7;j++){
         if(item[j]!='未知')
           item[j] = item[j].toFixed(2);
       }
 	  */
-      user_url = '/index/personal/?uid=' + item[0];
-      html += '<tr id=' + item[0] +'>';
-      html += '<td class="center" name="uids"><a href='+ user_url+ '  target="_blank">'+ item[0] +'</td>';
-      html += '<td class="center">'+ item[1] +'</td>';
-      html += '<td class="center">'+ item[2] +'</td>';
-      html += '<td class="center" style="width:100px;">'+ item[3] +'</td>';
-      html += '<td class="center" style="width:100px;">'+ item[4] +'</td>';
-      html += '<td class="center" style="width:100px;">'+ item[5] +'</td>';
-      html += '<td class="center" style="width:100px;">'+ item[6] +'</td>';
+      user_url = '/index/personal/?uid=' + item[i][0];
+      html += '<tr id=' + item[i][0] +'>';
+      html += '<td class="center" name="uids"><a href='+ user_url+ '  target="_blank">'+ item[i][0] +'</td>';
+      html += '<td class="center">'+ item[i][1] +'</td>';
+      html += '<td class="center">'+ item[i][2] +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[i][3].toFixed(2) +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[i][4].toFixed(2) +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[i][5].toFixed(2) +'</td>';
+	  html += '<td class="center" style="width:100px;">'+ item[i][6].toFixed(2) +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[i][7] +'</td>';
       html += '<td class="center" style="width:120px;"><a class="portrait_href" href=' + user_url + ' target="_blank">查看人物属性页</a></td>';
       html += '</tr>';
     }
     html += '</tbody>';
     html += '</table>';
     $('#search_result').append(html);
-   /* 
+    
     $('.datatable').dataTable({
         "sDom": "<'row'<'col-md-6'l ><'col-md-6'f>r>t<'row'<'col-md-12'i><'col-md-12 center-block'p>>",
         "sPaginationType": "bootstrap",
@@ -46,5 +48,5 @@ function draw_search_results(data){
             "sLengthMenu": "每页&nbsp; _MENU_ 条"
         }
     });
-   */
+   
 }
