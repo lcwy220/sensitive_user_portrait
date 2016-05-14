4b02b4d38226832c074daa8d0d3013e4aec7bca1
linhaobuaa <1257819385@qq.com>
2016-05-14 20:51:03 +0800

    yuankun

--- sensitive_user_portrait/imagine/imagine.py
+++ sensitive_user_portrait/imagine/imagine.py
@@ -2,45 +2,80 @@
 
 import sys
 import json
+import math
 from elasticsearch import Elasticsearch
-from sensitive_user_portrait.global_utils import es_sensitive_user_portrait as es
-"""
-reload(sys)
-sys.path.append('./../')
-from global_utils import es_sensitive_user_portrait as es
-"""
-shift_dict = {"sensitive_words_string": "敏感词", "topic_string": "话题", "psycho_status_string": "心理状态", \
-        'sensitive_geo_string': "地理位置", 'sensitive_hashtag_string': "hashtag", "domain": "领域"}
+from sensitive_user_portrait.global_utils import es_user_portrait as es
+from sensitive_user_portrait.global_utils import portrait_index_name, portrait_index_type
+#from sensitive_user_portrait.filter_uid import all_delete_uid
+from sensitive_user_portrait.global_utils import R_ADMIN
 
-def imagine(uid, query_fields_dict,index_name="sensitive_user_portrait", doctype='user'):
+#use to get evaluate max
+def get_evaluate_max():
+    max_result = {}
+    evaluate_index = ['influence', 'activeness', 'importance']
+    for evaluate in evaluate_index:
+        query_body = {
+            'query':{
+                'match_all':{}
+                    },
+                'size':1,
+                'sort':[{evaluate: {'order': 'desc'}}]
+                }
+        try:
+            result = es.search(index=portrait_index_name, doc_type=portrait_index_type, body=query_body)['hits']['hits']
+        except Exception, e:
+            raise e
+        max_evaluate = result[0]['_source'][evaluate]
+        max_result[evaluate] = max_evaluate
+    return max_result
 
-    """
-    uid: search users relate to uid
-    query_fields_dict: defined search field weight
-    fields: domain, topic, keywords, psycho_status, psycho_feature, activity_geo, hashtag
-    for example: "domain": 2
-    domain, psycho_feature
-    """
-    personal_info = es.get(index="sensitive_user_portrait", doc_type="user", id=uid, _source=True)['_source']
 
-    keys_list = query_fields_dict.keys()
-    keys_list.remove('field')
+def imagine(uid, query_fields_dict,index_name=portrait_index_name, doctype=portrait_index_type):
+    default_setting_dict = query_fields_dict
+
+    personal_info = es.get(index=portrait_index_name, doc_type=portrait_index_type, id=uid, _source=True)['_source']
+
+    user_tag = submit_user + "-tag"
+    user_tag_string = personal_info.get(user_tag, "")
+    if user_tag_string:
+        tag_pairs_list = user_tag_string.split('&')
+    else:
+        tag_pairs_list = []
+    tag_dict = dict()
+    if tag_pairs_list:
+        for item in tag_pairs_list:
+            iter_pair = item.split('-')
+            tag_dict[iter_pair[0]] = iter_pair[1]
+
+    keys_list = []
+    for k, v in query_fields_dict.iteritems():
+        if v:
+            keys_list.append(k) #需要进行关联的键
     keys_list.remove('size')
 
     search_dict = {}
     iter_list = []
+    tag_attri_vaule = []
 
+    # 对搜索的键值进行过滤，去掉无用的键
     for iter_key in keys_list:
-        if iter_key not in personal_info or personal_info[iter_key] == '':
-            query_fields_dict.pop(iter_key)
+        if iter_key in personal_info:
+            if not personal_info[iter_key] or not query_fields_dict[iter_key]:
+                query_fields_dict.pop(iter_key)
+                continue
+            else:
+                iter_list.append(iter_key)
+                temp = personal_info[iter_key]
+                search_dict[iter_key] = temp.split('&')
+
         else:
-            iter_list.append(iter_key)
-            temp = personal_info[iter_key]
-            search_dict[iter_key] = temp.split('&')
+            query_fields_dict.pop(iter_key)
+            if tag_dict.get(iter_key,''):
+                tag_attri_vaule.append(iter_key+"-"+tag_dict[iter_key])
+                
 
-    if len(iter_list) == 0:
+    if len(iter_list) == 0 and len(tag_attri_vaule) == 0:
         return []
-
     query_body = {
         'query':{
             'function_score':{
@@ -49,71 +84,59 @@ def imagine(uid, query_fields_dict,index_name="sensitive_user_portrait", doctype
                         'must':[
                         ]
                     }
-                },
-                "field_value_factor":{
                 }
             }
         }
     }
-
-    score_standard = {}
-    score_standard["modifier"] = "log1p"
-    if query_fields_dict['field'] == "activeness":
-        score_standard['field'] = "activeness"
-        score_standard['factor'] = 100
-    elif query_fields_dict['field'] == "importance":
-        score_standard['field'] = "importance"
-        score_standard['factor'] = 0.01
-    elif query_fields_dict['field'] == "sensitive":
-        score_standard['field'] = "sensitive"
-        score_standard['factor'] = 100
-    elif query_fields_dict['field'] == 'influence':
-        score_standard['field'] = "influence"
-        score_standard['factor'] = 0.1
-    else:
-        score_standard['field'] = "influence"
-        score_standard['factor'] = 0
-        query_body['query']['function_score']['boost_mode'] = "sum"
-
-    query_body['query']['function_score']['field_value_factor'] = score_standard
-
-    query_fields_dict.pop('field')
     number = es.count(index=index_name, doc_type=doctype, body=query_body)['count']
-    query_body['size'] = 100 # default number
+
+    query_body['size'] = 150 # default number
     query_number = query_fields_dict['size'] #  required number
     query_fields_dict.pop('size')
+
+    if tag_attri_vaule:
+        query_body['query']['function_score']['query']['bool']['must'].append({"terms":{user_tag:tag_attri_vaule}})
 
     for (k,v) in query_fields_dict.items():
 
         temp = {}
         temp_list = []
-        for iter_key in search_dict[k]:
-            temp_list.append({'wildcard':{k:{'wildcard':'*'+iter_key+'*','boost': v}}})
+        if k in personal_info and v != 0:
+            for iter_key in search_dict[k]:
+                temp_list.append({'wildcard':{k:{'wildcard':'*'+iter_key+'*', 'boost': v}}})
 
-        query_body['query']['function_score']['query']['bool']['must'].append({'bool':{'should':temp_list}})
+            query_body['query']['function_score']['query']['bool']['must'].append({'bool':{'should':temp_list}})
 
-
+    #filter_uid = all_delete_uid()
     result = es.search(index=index_name, doc_type=doctype, body=query_body)['hits']['hits']
-    field_list = ['uid','uname', 'activeness','importance', 'influence', 'sensitive']
+    field_list = ['uid','uname', 'activeness','importance', 'influence']
+    evaluate_index_list = ['activeness', 'importance', 'influence']
     return_list = []
     count = 0
+
+    if len(result) > 1 and result:
+        if result[0]['_id'] != uid:
+            top_score = result[0]['_score']
+        else:
+            top_score = result[1]['_score']
+
+    #get evaluate max to normal
+    evaluate_max_dict = get_evaluate_max()
     for item in result:
         if uid == item['_id']:
             score = item['_score']
             continue
         info = []
-        if not item['_source']['uname']:
-            item['_source']['uname'] = 'unknown'
         for field in field_list:
-            info.append(item['_source'][field])
-        info.append(item['_score'])
-        common_dict = dict()
-        for iter_key in iter_list:
-            iter_common_list = item['_source'][iter_key].split('&')
-            search_common_list = list(set(iter_common_list) & set(search_dict[iter_key]))   
-            iter_key = shift_dict[iter_key]
-            common_dict[iter_key] = search_common_list
-        info.append(common_dict)
+            if field in evaluate_index_list:
+                value = item['_source'][field]
+                normal_value = math.log(value / float(evaluate_max_dict[field] )* 9 + 1, 10) * 100
+            else:
+                normal_value = item['_source'][field]
+                if not normal_value:
+                    normal_value = item['_id']
+            info.append(normal_value)
+        info.append(item['_score']/float(top_score)*100)
         return_list.append(info)
         count += 1
 
@@ -124,11 +147,17 @@ def imagine(uid, query_fields_dict,index_name="sensitive_user_portrait", doctype
 
     temp_list = []
     for field in field_list:
-        temp_list.append(personal_info[field])
+        if field in evaluate_index_list:
+            value = personal_info[field]
+            normal_value = math.log(value / float(evaluate_max_dict[field]) * 9 + 1, 10) * 100
+        else:
+            normal_value = personal_info[field]
+        temp_list.append(normal_value)
 
     results = []
     results.append(temp_list)
     results.extend(return_list)
+    results.append(default_setting_dict)
 
 
     return results
@@ -137,5 +166,5 @@ def imagine(uid, query_fields_dict,index_name="sensitive_user_portrait", doctype
 
 
 if __name__ == '__main__':
-    print imagine('2803301701', {'sensitive_words_string':1, 'sensitive_geo_string':2,'field':'default','size':11}, index_name='sensitive_user_portrait', doctype='user')
+    print imagine(2010832710, {'topic':1, 'keywords':2,'field':'default','size':11}, index_name='test_user_portrait', doctype='user')
 
--- sensitive_user_portrait/imagine/views.py
+++ sensitive_user_portrait/imagine/views.py
@@ -5,16 +5,79 @@ import time
 import json
 from flask import Blueprint, url_for, render_template, request, abort, flash, session, redirect
 from imagine import imagine
+from sensitive_user_portrait.global_utils import es_sensitive_user_portrait as es
+from sensitive_user_portrait.global_utils import portrait_index_name, portrait_index_type
+
 
 mod = Blueprint('imagine', __name__, url_prefix='/imagine')
-shift_dict = {"sensitive_words": "sensitive_words_string", "activity_geo": "sensitive_geo_string", "hashtag": "sensitive_hashtag_string"}
+shift_dict = {"sensitive_words": "sensitive_words_string", "hashtag": "hashtag_string"}
 
-@mod.route('/')
+
+@mod.route('/portrait_related/')
+def ajax_portrait_related():
+    uid = request.args.get('uid', '') # uid
+    results = dict()
+
+    if uid:
+        portrait_result = es.get(index=portrait_index_name, doc_type=portrait_index_type, id=uid)['_source']
+        results["domain"] = portrait_result['domain']
+        results["topic"] = portrait_result["topic_string"].replace("&", " ")
+        results["politics"] = portrait_result["politics"]
+        sensitive_words_dict = json.loads(portrait_result["sensitive_words_dict"])
+        if sensitive_words_dict:
+            sorted_sensitive_words = sorted(sensitive_words_dict.items(), key=lambda x:x[1], reverse=True)
+            tmp = sorted_sensitive_words[:3]
+            sensitive_words_list = [item[0] for item in tmp]
+            results["sensitive_words_string"] = " ".join(sensitive_words_list)
+            results["sensitive_words_detail"] = sorted_sensitive_words
+        else:
+            results["sensitive_words_string"] = ""
+            results["sensitive_words_detail"] = []
+        keywords_dict = json.loads(portrait_result["keywords_dict"])
+        results["keywords_detail"] = keywords_dict
+        if keywords_dict:
+            tmp = keywords_dict[:3]
+            keywords_list = [item[0] for item in tmp]
+            results["keywords_string"] = " ".join(keywords_list)
+        else:
+            results["keywords_string"] = ""
+        activity_geo_dict = json.loads(portrait_result["activity_geo_dict"])
+        geo_dict = {}
+        for item in activity_geo_dict:
+            for k, v in item.iteritems():
+                if geo_dict.has_key(k):
+                    geo_dict[k] += v
+                else:
+                    geo_dict[k] = v
+        if geo_dict:
+            sorted_geo_dict = sorted(geo_dict.items(), key=lambda x:x[1], reverse=True)
+            results["geo_activity"] = sorted_geo_dict[0][0]
+            results["geo_activity_detail"] = sorted_geo_dict
+        else:
+            results["geo_activity"] = ""
+            results["geo_activity_detail"] = []
+        hashtag_dict = json.loads(portrait_result["hashtag_dict"])
+        sorted_hashtag_dict = sorted(hashtag_dict.items(), key=lambda x:x[1], reverse=True)
+        if sorted_hashtag_dict:
+            results["hashtag"] = sorted_hashtag_dict[0][0]
+            results["hashtag_detail"] = sorted_hashtag_dict
+        else:
+            results["hashtag"] = ""
+            results["hashtag_detail"] = []
+
+    return json.dumps(results)
+
+
+
+# politics, sensitive_words_string, domain, topic_string, hashtag_string, activity_geo,keywords_string
+@mod.route('/imagine/')
 def ajax_imagine():
     uid = request.args.get('uid', '') # uid
     query_keywords = request.args.get('keywords','') # query dict and corresponding weight
-    keywords_list = query_keywords.split(',')
     query_weight = request.args.get('weight','')
+    size = request.args.get('size', 100)
+    order = request.args.get('order', "fansnum")
+    keywords_list = query_keywords.split(',')
     weight_list = query_weight.split(',')
 
     if len(keywords_list) != len(weight_list):
@@ -22,14 +85,8 @@ def ajax_imagine():
 
     query_fields_dict = {}
     for i in range(len(keywords_list)):
-        if shift_dict.has_key(keywords_list[i]):
-            keywords_list[i] = shift_dict[str(keywords_list[i])]
         query_fields_dict[keywords_list[i]] = int(weight_list[i])
 
-    field = request.args.get('field', '')
-    query_fields_dict['field'] = field
-
-    size = request.args.get('size', 15)
     query_fields_dict['size'] = int(size)
     if uid and query_fields_dict:
         result = imagine(uid, query_fields_dict)
